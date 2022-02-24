1669267179b8982f448c8e08b6c359f003262731
Kostas Stamatis <kstamatis@ekt.gr>
2022-02-24 23:08:19 +0200

    add json support to the endpoint of eag response

--- Dashboard/src/main/java/eu/apenet/api/eag/EagApiAction.java
+++ Dashboard/src/main/java/eu/apenet/api/eag/EagApiAction.java
@@ -1,20 +1,29 @@
 package eu.apenet.api.eag;
 
 import com.opensymphony.xwork2.Action;
+import eu.apenet.commons.ResourceBundleSource;
 import eu.apenet.commons.StrutsResourceBundleSource;
 import eu.apenet.commons.utils.APEnetUtilities;
+import eu.apenet.commons.xslt.ClasspathURIResolver;
 import eu.apenet.commons.xslt.EagXslt;
+import eu.apenet.commons.xslt.extensions.ResourcebundleExtension;
+import eu.apenet.commons.xslt.extensions.RetrieveRelatedAIIdExtension;
 import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
 import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.vo.ArchivalInstitution;
-import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.*;
 
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.*;
 
 public class EagApiAction {
     private String html = "";
+    private String json = "";
 
     private String aiId;
     private Boolean preview = false;
@@ -38,7 +47,36 @@ public class EagApiAction {
 
         this.html = stringWriter.toString();
 
+        String input = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><temp>"+stringWriter.toString()+"</temp>";
+
+        Source xmlSource = new StreamSource(new ByteArrayInputStream(input.getBytes()));
+        String xslt = "xsl/xml-to-json.xsl";
+        try {
+            XsltExecutable executable = getXsltExecutable(xslt);
+            XsltTransformer transformer = executable.load();
+            transformer.setSource(xmlSource);
+            Serializer serializer = new Serializer();
+            StringWriter jsonStringWriter = new StringWriter();
+            serializer.setOutputWriter(jsonStringWriter);
+            transformer.setDestination(serializer);
+            transformer.transform();
+            String response = jsonStringWriter.toString();
+            this.json = response;
+        } catch (SaxonApiException e) {
+            e.printStackTrace();
+        }
+
         return Action.SUCCESS;
+    }
+
+    private static XsltExecutable getXsltExecutable(String xslUrl) throws SaxonApiException {
+        ClassLoader classLoader = (ClassLoader) Thread.currentThread().getContextClassLoader();
+        Source xsltSource = new StreamSource(classLoader.getResourceAsStream(xslUrl));
+        Processor processor = new Processor(false);
+        XsltCompiler compiler = processor.newXsltCompiler();
+        compiler.setURIResolver(new ClasspathURIResolver(xslUrl));
+
+        return compiler.compile(xsltSource);
     }
 
     public void setHtml(String html) {
@@ -63,5 +101,13 @@ public class EagApiAction {
 
     public void setPreview(Boolean preview) {
         this.preview = preview;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    public String getJson() {
+        return json;
     }
 }
