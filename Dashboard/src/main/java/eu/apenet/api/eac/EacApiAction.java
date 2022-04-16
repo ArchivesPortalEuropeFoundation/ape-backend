56be6742537fbdf98c3cf191ab0edcb5c6f20f1f
Kostas Stamatis <kstamatis@ekt.gr>
2022-04-16 17:58:58 +0300

    endpoint for eac html xslt transformation

---
+++ Dashboard/src/main/java/eu/apenet/api/eac/EacApiAction.java
@@ -0,0 +1,206 @@
+package eu.apenet.api.eac;
+
+import com.opensymphony.xwork2.Action;
+import eu.apenet.commons.StrutsResourceBundleSource;
+import eu.apenet.commons.solr.SolrField;
+import eu.apenet.commons.utils.APEnetUtilities;
+import eu.apenet.commons.xslt.eac.EacXslt;
+import eu.apenet.persistence.dao.EacCpfDAO;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.ArchivalInstitution;
+import eu.apenet.persistence.vo.EacCpf;
+import net.sf.saxon.s9api.SaxonApiException;
+import org.apache.commons.lang.StringUtils;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EacApiAction {
+    private String html = "";
+
+    private static final List<SolrField> DEFAULT_HIGHLIGHT_FIELDS = SolrField.getDefaults();
+    public static final String EACCPFDETAILS_XSLT = "eaccpfdetails";
+    public static final String EACCPFDETAILS_PREVIEW_XSLT = "eaccpfdetailspreview";
+
+    private String eaccpfId;
+    private String aiName;
+    private Boolean preview = false;
+    private String element;
+    private String term;
+    private String translationLanguage = "default";
+    private String langNavigator = "en";
+    private String aiCodeUrl = "http://localhost:8080/web/guest/directory/-/dir/ai/code";
+    private String eacUrlBase = "http://localhost:8080/web/guest/eac-display/-/eac/pl";
+    private String eadUrl = "http://localhost:8080/web/guest/ead-display/-/ead/pl";
+    private String secondDisplayUrl = null;
+
+    private final static Map<String, String> xsltUrls = new HashMap<String,String>();
+    static {
+        xsltUrls.put(EACCPFDETAILS_XSLT, "xsl/eaccpf/eaccpfdetails.xsl");
+        xsltUrls.put(EACCPFDETAILS_PREVIEW_XSLT,"xsl/eaccpf/eaccpfdetails-preview.xsl");
+    }
+
+    public EacApiAction() {
+
+    }
+
+    public String execute() throws IOException, SaxonApiException {
+
+        EacCpfDAO eacCpfDAO = DAOFactory.instance().getEacCpfDAO();
+        EacCpf eaccpf = eacCpfDAO.getEacCpfByIdentifier(aiName, eaccpfId,!preview);
+        ArchivalInstitution archivalInstitution = eaccpf.getArchivalInstitution();
+
+        File file= new File(APEnetUtilities.getApePortalAndDashboardConfig().getRepoDirPath() + eaccpf.getPath());
+        if (file.exists()){
+            FileReader eacFile = new FileReader(file);
+            Source xmlSource = new StreamSource(new StringReader(this.readFile(eacFile)));
+            List<SolrField> highlightFields = SolrField.getSolrFieldsByIdString(element);
+            if (highlightFields.size() == 0) {
+                highlightFields = DEFAULT_HIGHLIGHT_FIELDS;
+            }
+
+            Integer aiIdInt = archivalInstitution.getAiId();
+//            if (StringUtils.isNotBlank(archivalInstitution.getAiId())) {
+//                aiIdInt = Integer.parseInt(aiId);
+//            }
+            String xslLocation = xsltUrls.get(preview?EACCPFDETAILS_PREVIEW_XSLT:EACCPFDETAILS_XSLT);
+            if (xslLocation == null){
+//                LOG.warn("EAC-CPF xsl type does not exist: " + getType());
+            }else {
+                StringWriter stringWriter = new StringWriter();
+
+                EacXslt.convertEacToHtml(xslLocation, stringWriter, xmlSource, term,
+                        highlightFields, new StrutsResourceBundleSource(), secondDisplayUrl, aiIdInt, getPreview(),
+                        /*APEnetUtilities.getApePortalConfig().getSolrStopwordsUrl()*/null, this.getTranslationLanguage(), this.getAiCodeUrl(),
+                        this.getEacUrlBase(), this.getEadUrl(), this.getLangNavigator());
+                this.html = stringWriter.toString();
+            }
+        }
+
+        return Action.SUCCESS;
+    }
+
+    private String readFile(FileReader file) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(file);
+        String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String lineSeparator = System.getProperty("line.separator");
+
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(lineSeparator);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public void setEaccpfId(String eaccpfId) {
+        this.eaccpfId = eaccpfId;
+    }
+
+    public void setAiName(String aiName) {
+        this.aiName = aiName;
+    }
+
+    public void setPreview(Boolean preview) {
+        this.preview = preview;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public void setTranslationLanguage(String translationLanguage) {
+        this.translationLanguage = translationLanguage;
+    }
+
+    public void setLangNavigator(String langNavigator) {
+        this.langNavigator = langNavigator;
+    }
+
+    public void setAiCodeUrl(String aiCodeUrl) {
+        this.aiCodeUrl = aiCodeUrl;
+    }
+
+    public void setEacUrlBase(String eacUrlBase) {
+        this.eacUrlBase = eacUrlBase;
+    }
+
+    public void setEadUrl(String eadUrl) {
+        this.eadUrl = eadUrl;
+    }
+
+    public String getEaccpfId() {
+        return eaccpfId;
+    }
+
+    public String getAiName() {
+        return aiName;
+    }
+
+    public Boolean getPreview() {
+        return preview;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public String getTranslationLanguage() {
+
+        if (translationLanguage.equals("default") || translationLanguage.equals("showAll")){
+            return translationLanguage;
+        }
+        Map<String, String> langMap = APEnetUtilities.getIso2ToIso3LanguageCodesMap();
+
+        String langTransIso3 = "eng";
+        if (langMap.get(translationLanguage)!= null && !langMap.get(translationLanguage).isEmpty()){
+            langTransIso3 = langMap.get(translationLanguage);
+        }
+        return langTransIso3;
+    }
+
+    public String getLangNavigator() {
+        return langNavigator;
+    }
+
+    public String getAiCodeUrl() {
+        return aiCodeUrl;
+    }
+
+    public String getEacUrlBase() {
+        return eacUrlBase;
+    }
+
+    public String getEadUrl() {
+        return eadUrl;
+    }
+
+    public void setSecondDisplayUrl(String secondDisplayUrl) {
+        this.secondDisplayUrl = secondDisplayUrl;
+    }
+
+    public String getSecondDisplayUrl() {
+        return secondDisplayUrl;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+}
---
+++ commons/src/main/java/eu/apenet/commons/xslt/extensions/RetrieveRepositoryCodeFromEadIdExtension.java
@@ -0,0 +1,198 @@
+package eu.apenet.commons.xslt.extensions;
+
+import eu.apenet.persistence.dao.CLevelDAO;
+import eu.apenet.persistence.dao.Ead3DAO;
+import eu.apenet.persistence.dao.EadDAO;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.CLevel;
+import eu.apenet.persistence.vo.Ead;
+import eu.apenet.persistence.vo.Ead3;
+import eu.apenet.persistence.vo.FindingAid;
+import eu.apenet.persistence.vo.HoldingsGuide;
+import eu.apenet.persistence.vo.SourceGuide;
+import eu.archivesportaleurope.util.ApeUtil;
+import net.sf.saxon.expr.XPathContext;
+import net.sf.saxon.lib.ExtensionFunctionCall;
+import net.sf.saxon.lib.ExtensionFunctionDefinition;
+import net.sf.saxon.om.Sequence;
+import net.sf.saxon.om.StructuredQName;
+import net.sf.saxon.trans.XPathException;
+import net.sf.saxon.value.SequenceType;
+import net.sf.saxon.value.StringValue;
+
+/**
+ * Class for retrieve the identifier of the Archival Institution from the
+ * identifier of an apeEAD.
+ */
+public class RetrieveRepositoryCodeFromEadIdExtension extends ExtensionFunctionDefinition {
+
+    /**
+     * Serializable.
+     */
+    private static final long serialVersionUID = -5996799246531865681L;
+
+    /**
+     * Name of the function to call.
+     */
+    private static final StructuredQName funcname = new StructuredQName("ape", "http://www.archivesportaleurope.eu/xslt/extensions",
+            "aiFromEad");
+
+    private RetrieveRepositoryCodeFromEadIdCall retrieveRepositoryCodeFromEadIdCall;
+
+    public RetrieveRepositoryCodeFromEadIdExtension() {
+        this.retrieveRepositoryCodeFromEadIdCall = new RetrieveRepositoryCodeFromEadIdCall();
+    }
+
+    @Override
+    public SequenceType[] getArgumentTypes() {
+        return new SequenceType[]{SequenceType.OPTIONAL_STRING};
+    }
+
+    @Override
+    public StructuredQName getFunctionQName() {
+        return RetrieveRepositoryCodeFromEadIdExtension.funcname;
+    }
+
+    @Override
+    public SequenceType getResultType(SequenceType[] arg0) {
+        return SequenceType.OPTIONAL_STRING;
+    }
+
+    @Override
+    public ExtensionFunctionCall makeCallExpression() {
+        return this.retrieveRepositoryCodeFromEadIdCall;
+    }
+
+    public int getMinimumNumberOfArguments() {
+        return 2;
+    }
+
+    public int getMaximumNumberOfArguments() {
+        return 2;
+    }
+
+    class RetrieveRepositoryCodeFromEadIdCall extends ExtensionFunctionCall {
+
+        /**
+         * Serializable.
+         */
+        private static final long serialVersionUID = 864847688629772008L;
+
+        /**
+         * Constructor.
+         */
+        public RetrieveRepositoryCodeFromEadIdCall() {
+            super();
+        }
+
+        @Override
+        public Sequence call(XPathContext xPathContext, Sequence[] sequences) throws XPathException {
+            if (sequences != null && sequences.length == 2) {
+                String firstArgValue = sequences[0].head().getStringValue();
+                String secondArgValue = sequences[1].head().getStringValue();
+                String value = "";
+                String type = "";
+
+                // apeEAD and EAD3 lookup.
+                EadDAO eadDAO = DAOFactory.instance().getEadDAO();
+                Ead ead = null;
+                Ead3DAO ead3DAO = DAOFactory.instance().getEad3DAO();
+                Ead3 ead3 = null;
+                CLevelDAO clevelDAO = DAOFactory.instance().getCLevelDAO();
+                CLevel clevel = null;
+
+                if (firstArgValue != null && !firstArgValue.isEmpty()) {
+                    if (firstArgValue.startsWith("ead3:")) {
+                        String ead3recordid = firstArgValue.substring(5);
+                        String unitid = null;
+                        if (ead3recordid.contains("::")) {
+                            unitid = ead3recordid.replace("::", "-");
+                            ead3recordid = ead3recordid.split("::")[0];
+                        }
+                        // Checks if a EAD3 with the provided identifier exists.
+                        if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                            // And repository code.
+                            ead3 = ead3DAO.getEad3ByIdentifier(secondArgValue, ead3recordid, true);
+                            if (unitid != null){
+                                clevel = clevelDAO.getCLevelByUnitId(unitid, secondArgValue, ead3recordid);
+                            }
+                        } else {
+                            // First FA with identifier.
+                            ead3 = ead3DAO.getFirstPublishedEad3ByIdentifier(ead3recordid, true);
+                            if (unitid != null){
+                                clevel = clevelDAO.getCLevelByUnitId(unitid, ead3.getId().toString());
+                            }
+                        }
+                        type = "ead3";
+                    } else {
+                        // Checks if exists a FA with the provided identifier.
+                        if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                            // And repository code.
+                            ead = eadDAO.getEadByEadid(FindingAid.class, secondArgValue, firstArgValue, true);
+                            type = "fa";
+                        } else {
+                            // First FA with identifier.
+                            ead = eadDAO.getFirstPublishedEadByEadid(FindingAid.class, firstArgValue);
+                            type = "fa";
+                        }
+
+                        if (ead == null) {
+                            // Checks if exists a HG with the provided identifier.
+                            if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                                // And repository code.
+                                ead = eadDAO.getEadByEadid(HoldingsGuide.class, secondArgValue, firstArgValue, true);
+                                type = "hg";
+                            } else {
+                                // First HG with identifier.
+                                ead = eadDAO.getFirstPublishedEadByEadid(HoldingsGuide.class, firstArgValue);
+                                type = "hg";
+                            }
+                        }
+
+                        if (ead == null) {
+                            // Checks if exists a SG with the provided identifier.
+                            if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                                // And repository code.
+                                ead = eadDAO.getEadByEadid(SourceGuide.class, secondArgValue, firstArgValue, true);
+                                type = "sg";
+                            } else {
+                                // First SG with identifier.
+                                ead = eadDAO.getFirstPublishedEadByEadid(SourceGuide.class, firstArgValue);
+                                type = "sg";
+                            }
+                        }
+                    }
+                }
+
+                if (ead != null) {
+                    String repositoryCode = "";
+                    if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                        repositoryCode = secondArgValue;
+                    } else {
+                        repositoryCode = ead.getArchivalInstitution().getRepositorycode();
+                    }
+
+                    value = "aicode/" + repositoryCode + "/type/" + type + "/id/" + ApeUtil.encodeSpecialCharacters(ead.getEadid());
+                } else if (ead3 != null) {
+                    String repositoryCode = "";
+                    if (secondArgValue != null && !secondArgValue.isEmpty()) {
+                        repositoryCode = secondArgValue;
+                    } else {
+                        repositoryCode = ead3.getArchivalInstitution().getRepositorycode();
+                    }
+
+                    value = "aicode/" + repositoryCode + "/type/" + type + "/id/" + ApeUtil.encodeSpecialCharacters(ead3.getIdentifier());
+                    if (clevel != null) {
+                        value = value.concat("/unitid/" + ApeUtil.encodeSpecialCharacters(clevel.getUnitid()));
+                    }
+                }
+
+                return StringValue.makeStringValue(value);
+            } else {
+                return StringValue.makeStringValue("ERROR");
+            }
+        }
+
+    }
+
+}
