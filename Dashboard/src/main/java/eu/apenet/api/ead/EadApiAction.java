58a13b57e59cc360ffbcb470609dd2789e48681b
Kostas Stamatis <kstamatis@ekt.gr>
2022-05-02 17:49:10 +0300

    endpoint for ead html xslt transformation

---
+++ Dashboard/src/main/java/eu/apenet/api/ead/EadApiAction.java
@@ -0,0 +1,242 @@
+package eu.apenet.api.ead;
+
+import com.opensymphony.xwork2.Action;
+import eu.apenet.commons.StrutsResourceBundleSource;
+import eu.apenet.commons.solr.SolrField;
+import eu.apenet.commons.types.XmlType;
+import eu.apenet.commons.utils.APEnetUtilities;
+import eu.apenet.commons.xslt.eac.EacXslt;
+import eu.apenet.commons.xslt.ead.EadXslt;
+import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
+import eu.apenet.persistence.dao.EacCpfDAO;
+import eu.apenet.persistence.dao.EadDAO;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.*;
+import net.sf.saxon.s9api.SaxonApiException;
+import org.apache.commons.lang.StringUtils;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EadApiAction {
+    private String html = "";
+
+    private static final List<SolrField> DEFAULT_HIGHLIGHT_FIELDS = SolrField.getDefaults();
+    public static final String CDETAILS_CHILD_XSLT = "cdetails-child";
+    public static final String FRONTPAGE_XSLT = "frontpage";
+    public static final String CDETAILS_XSLT = "cdetails";
+
+
+    private String element;
+    private String term;
+    private String type;
+    private String xmlType;
+    private String preview = "false";
+    private String dashboardPreview = "false";
+    private String eacUrlBase = "http://localhost:8080/web/guest/eac-display/-/eac/pl";
+    private String secondDisplayUrl = null;
+
+    private String aiId;
+    private String aiName;
+    private String aiRepositoryCode;
+
+    String eadid;
+
+    private final static Map<String, String> xsltUrls = new HashMap<String,String>();
+    static {
+        xsltUrls.put(CDETAILS_XSLT, "xsl/ead/cdetails.xsl");
+        xsltUrls.put(CDETAILS_CHILD_XSLT, "xsl/ead/cdetails-child.xsl");
+        xsltUrls.put(FRONTPAGE_XSLT, "xsl/ead/frontpage.xsl");
+    }
+
+    public EadApiAction() {
+
+    }
+
+    public String execute() throws IOException, SaxonApiException {
+        HttpServletResponse response = ServletActionContext.getResponse();
+
+        ArchivalInstitutionDAO archivalInstitutionDAO = DAOFactory.instance().getArchivalInstitutionDAO();
+        if (aiId != null){
+            ArchivalInstitution archivalInstitution = archivalInstitutionDAO.findById(Integer.parseInt(aiId));
+            aiRepositoryCode = archivalInstitution.getRepositorycode();
+            aiName = archivalInstitution.getAiname();
+        }
+        else if (aiName != null){
+            ArchivalInstitution archivalInstitution = archivalInstitutionDAO.getArchivalInstitutionByAiName(aiName);
+            aiRepositoryCode = archivalInstitution.getRepositorycode();
+            aiId = ""+archivalInstitution.getAiId();
+        }
+        else {
+            ArchivalInstitution archivalInstitution = archivalInstitutionDAO.getArchivalInstitutionByRepositoryCode(aiRepositoryCode);
+            aiId = ""+archivalInstitution.getAiId();
+            aiName = archivalInstitution.getAiname();
+        }
+
+        XmlType xmlType = XmlType.getTypeByResourceName(getXmlType());
+
+        EadDAO eadDAO = DAOFactory.instance().getEadDAO();
+        Ead ead = eadDAO.getEadByEadid(xmlType.getEadClazz(), Integer.parseInt(aiId), eadid);
+        EadContent eadContent =  ead.getEadContent();
+
+        Source xmlSource = new StreamSource(new StringReader(eadContent.getXml()));
+        List<SolrField> highlightFields = SolrField.getSolrFieldsByIdString(element);
+        if (highlightFields.size() == 0) {
+            highlightFields = DEFAULT_HIGHLIGHT_FIELDS;
+        }
+        try {
+            Integer aiIdInt = null;
+            if (StringUtils.isNotBlank(aiId)) {
+                aiIdInt = Integer.parseInt(aiId);
+            }
+            String xslLocation = xsltUrls.get(getType());
+            if (xslLocation == null) {
+//                LOG.warn("EAD xsl type does not exist: " + getType());
+            } else {
+                String typeOfDisplay = "normal";
+                if ("true".equalsIgnoreCase(getPreview())) {
+                    typeOfDisplay = "preview";
+                } else if (CDETAILS_CHILD_XSLT.equalsIgnoreCase(getType())) {
+                    typeOfDisplay = "child";
+                }
+
+                StringWriter stringWriter = new StringWriter();
+                EadXslt.convertEadToHtml(xslLocation, stringWriter, xmlSource, term,
+                        highlightFields, new StrutsResourceBundleSource(), secondDisplayUrl, aiIdInt,
+                        "true".equalsIgnoreCase(getDashboardPreview()), APEnetUtilities.getApePortalConfig().getSolrStopwordsUrl(),
+                        typeOfDisplay, xmlType, this.getEacUrlBase());
+                this.html = stringWriter.toString();
+            }
+        } catch (Exception e) {
+//            LOG.error(e.getMessage(), e);
+        }
+
+        response.setHeader("Access-Control-Allow-Origin","*");
+        return Action.SUCCESS;
+    }
+
+    private String readFile(FileReader file) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(file);
+        String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String lineSeparator = System.getProperty("line.separator");
+
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(lineSeparator);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public void setAiName(String aiName) {
+        this.aiName = aiName;
+    }
+
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+
+    public String getPreview() {
+        return preview;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getAiName() {
+        return aiName;
+    }
+
+
+    public String getElement() {
+        return element;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public String getEacUrlBase() {
+        return eacUrlBase;
+    }
+
+    public void setSecondDisplayUrl(String secondDisplayUrl) {
+        this.secondDisplayUrl = secondDisplayUrl;
+    }
+
+    public String getSecondDisplayUrl() {
+        return secondDisplayUrl;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setAiId(String aiId) {
+        this.aiId = aiId;
+    }
+
+    public void setAiRepositoryCode(String aiRepositoryCode) {
+        this.aiRepositoryCode = aiRepositoryCode;
+    }
+
+    public String getAiId() {
+        return aiId;
+    }
+
+    public String getAiRepositoryCode() {
+        return aiRepositoryCode;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setDashboardPreview(String dashboardPreview) {
+        this.dashboardPreview = dashboardPreview;
+    }
+
+    public String getDashboardPreview() {
+        return dashboardPreview;
+    }
+
+    public void setXmlType(String xmlType) {
+        this.xmlType = xmlType;
+    }
+
+    public String getXmlType() {
+        return xmlType;
+    }
+
+    public void setEacUrlBase(String eacUrlBase) {
+        this.eacUrlBase = eacUrlBase;
+    }
+
+    public void setEadid(String eadid) {
+        this.eadid = eadid;
+    }
+
+    public String getEadid() {
+        return eadid;
+    }
+}
---
+++ commons/src/main/java/eu/apenet/commons/xslt/ead/EadXslt.java
@@ -0,0 +1,66 @@
+package eu.apenet.commons.xslt.ead;
+
+import java.io.Writer;
+import java.util.List;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import eu.apenet.commons.xslt.extensions.*;
+import eu.apenet.commons.ResourceBundleSource;
+import eu.apenet.commons.solr.SolrField;
+import eu.apenet.commons.types.XmlType;
+import eu.apenet.commons.xslt.ClasspathURIResolver;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.QName;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.Serializer;
+import net.sf.saxon.s9api.XdmAtomicValue;
+import net.sf.saxon.s9api.XsltCompiler;
+import net.sf.saxon.s9api.XsltExecutable;
+import net.sf.saxon.s9api.XsltTransformer;
+
+
+public final class EadXslt {
+
+    private static XsltExecutable getXsltExecutable(String xslUrl, String searchTerms, List<SolrField> highlightFields, ResourceBundleSource resourceBundleSource,
+                                                    Integer aiId, boolean isDashboard, String solrStopwordsUrl, String typeOfDisplay, XmlType xmlType) throws SaxonApiException {
+        ClassLoader classLoader = (ClassLoader) Thread.currentThread().getContextClassLoader();
+        Source xsltSource = new StreamSource(classLoader.getResourceAsStream(xslUrl));
+        Processor processor = new Processor(false);
+        HighlighterExtension highLighter = new HighlighterExtension(solrStopwordsUrl, searchTerms, highlightFields);
+        ResourcebundleExtension resourcebundleRetriever = new ResourcebundleExtension(resourceBundleSource);
+        EadidCheckerExtension eadidChecker = new EadidCheckerExtension(aiId, isDashboard);
+        EacRecordidCheckerExtension eacRecordidChecker = new EacRecordidCheckerExtension(aiId, isDashboard);
+        CheckAgencyCodeExtension getAiCode1 = new CheckAgencyCodeExtension();
+        RetrieveRepositoryCodeFromEacIdExtension getAiCode2 = new RetrieveRepositoryCodeFromEacIdExtension();
+        processor.registerExtensionFunction(highLighter);
+        processor.registerExtensionFunction(resourcebundleRetriever);
+        processor.registerExtensionFunction(eadidChecker);
+        processor.registerExtensionFunction(eacRecordidChecker);
+        processor.registerExtensionFunction(getAiCode1);
+        processor.registerExtensionFunction(getAiCode2);
+        processor.registerExtensionFunction(new SpecialCharactersEncoderExtension());
+        processor.registerExtensionFunction(new TypeOfDisplayExtension(typeOfDisplay));
+        processor.registerExtensionFunction(new OtherFindingAidExtension(resourceBundleSource, xmlType));
+        XsltCompiler compiler = processor.newXsltCompiler();
+        compiler.setURIResolver(new ClasspathURIResolver(xslUrl));
+        return compiler.compile(xsltSource);
+    }
+
+    public static void convertEadToHtml(String xslUrl, Writer writer, Source xmlSource, String searchTerms, List<SolrField> highlightFields,
+            ResourceBundleSource resourceBundleSource, String secondDisplayUrl, Integer aiId, boolean isDashboard,
+            String solrStopwordsUrl, String typeOfDisplay, XmlType xmlType, String eacUrl) throws SaxonApiException {
+        XsltExecutable executable = getXsltExecutable(xslUrl, searchTerms, highlightFields, resourceBundleSource, aiId, isDashboard, solrStopwordsUrl, typeOfDisplay, xmlType);
+        XsltTransformer transformer = executable.load();
+        transformer.setParameter(new QName("eadcontent.extref.prefix"), new XdmAtomicValue(secondDisplayUrl));
+        transformer.setParameter(new QName("eacUrl"), new XdmAtomicValue(eacUrl));
+        transformer.setSource(xmlSource);
+        Serializer serializer = new Serializer();
+        serializer.setOutputWriter(writer);
+        transformer.setDestination(serializer);
+        transformer.transform();
+    }
+
+}
