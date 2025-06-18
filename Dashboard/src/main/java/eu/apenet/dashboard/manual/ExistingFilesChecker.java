218e343c2f8645d1c8d0e69222f30823911da7b4
Kostas Stamatis <k.i.stamatis@gmail.com>
2025-06-18 11:27:01 +0300

    Fix issue with parsing XML files with wrong DTD url

--- Dashboard/src/main/java/eu/apenet/dashboard/manual/ExistingFilesChecker.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/manual/ExistingFilesChecker.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
@@ -1196,6 +1197,16 @@ public class ExistingFilesChecker {
         factory.setValidating(false);
         factory.setNamespaceAware(true);
         factory.setIgnoringElementContentWhitespace(true);
+
+        factory.setNamespaceAware(true);
+        try {
+            factory.setFeature("http://xml.org/sax/features/namespaces", false);
+            factory.setFeature("http://xml.org/sax/features/validation", false);
+            factory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+            factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
 
         // Try to recover the current EADID in the file.
         String oldIdentifier = "";
